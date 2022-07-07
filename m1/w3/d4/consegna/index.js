939201e21d1fc44b78714a273569859141041878
Edotrova <trovarelliedoardo@gmail.com>
2022-07-07 15:54:10 +0200

    w3 d5 consegna

---
+++ m1/w3/d4/consegna/index.js
@@ -0,0 +1,70 @@
+
+    //punto1
+
+
+let titolo = 'Corso di Javacript'
+
+let maius = titolo.toUpperCase();
+console.log(maius);
+
+let minus = titolo.toLowerCase();
+console.log(minus);
+
+
+        //punto2
+
+let divisione = titolo.split('');
+console.log(divisione);
+
+let nuovoTitolo2 =(divisione[0]) + (divisione[2]);
+console.log(nuovoTitolo);
+
+
+        //punto3
+
+
+let gruppo = ["Fabiola", "Edoardo", "Luigi", "Giuseppe"]
+
+console.log(gruppo[2]);
+
+let amici = Alessandro
+gruppo.push(amici);
+console.log(gruppo);
+
+console.log(gruppo.length);
+
+//punto4 
+
+
+
+
+function sum(){
+
+    let numeri = [2, 6, 10];
+    return numeri[0]+ numeri [1] + numeri[2];
+
+}
+
+sommma()
+console.log(somma())
+
+
+//punto5
+
+
+let frutta = ['banana', 'fragola', 'mela', 'anguria']
+
+let eliminaUltimo = frutta.pop();
+  console.log(eliminaUltimo);
+    
+
+let aggiungiUltimo = frutta.push("melone")
+    console.log(aggiungiUltimo);
+    console.log(frutta)
+
+ let eliminaPrimo = frutta.shift ();
+    console.group(eliminaPrimo);
+
+ let aggiungPrimo = frutta.unishift (cocco);
+    console.log(aggiungiPrimo);
+    console.log(frutta);
