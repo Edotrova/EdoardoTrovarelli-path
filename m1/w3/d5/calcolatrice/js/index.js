2653b72393c44485b490b1f9d64627e094a88f42
Edotrova <trovarelliedoardo@gmail.com>
2022-07-08 16:26:41 +0200

    w3 d5 calcolatrice

---
+++ m1/w3/d5/calcolatrice/js/index.js
@@ -0,0 +1,37 @@
+
+
+function calcola() {
+    let display = document.getElementById("display");
+    let totale = eval(display.value)
+    display.value = totale
+}
+
+function cancella() {
+
+    display.value = ""
+
+}
+
+function aggiungiCarattere(bottone) {
+    let display = document.getElementById("display");
+    let simbolo = bottone.getAttribute('carattere');
+
+    console.log(bottone.innerHTML)
+    console.log(bottone.getAttribute('carattere'))
+
+    display.value += bottone.getAttribute('carattere')
+
+}
+
+function isSymbol(carattere) {
+    const simboli = ['*', '/', '+', '-'];
+
+    if (simboli.includes(carattere)) {
+        return true;
+    }
+    return false;
+
+}
+
+
+
