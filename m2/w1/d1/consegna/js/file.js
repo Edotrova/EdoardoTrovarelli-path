3fb47e104ab9e32f088921f6195c422e664157c7
Edotrova <trovarelliedoardo@gmail.com>
2022-07-18 16:41:43 +0200

    aggiornamento1807consegna

--- m2/w1/d1/consegna/js/file.js
+++ m2/w1/d1/consegna/js/file.js
@@ -1,47 +1,78 @@
 
 let data = new Date()
 
-data;
+// metodo 1
 
+data;
 console.log(data)
 
-let dataattuale = document.getElementById("data-attuale")
-dataattuale.innerHTML = data
 
-console.log (data.getHours())
+let dataAttuale = document.getElementById('data-attuale')
+dataAttuale.innerHTML = data
 
-console.log (data.getMinutes())
 
-console.log (data.getSeconds())
+// metodo 2
 
-console.log (data.getFullYear())
+
+let dataAttuale2 = document.getElementById('data-attuale2')
+dataAttuale2.innerHTML = data.toLocaleDateString()
+
+
+
+// mostrare ogni elemento
+
+console.log(data.getDate())
+console.log(data.getMonth())
+console.log(data.getFullYear())
+console.log(data.getHours())
+console.log(data.getMinutes())
+console.log(data.getSeconds())
+
+
+
+//mostare ora in tempo reale
+
+setInterval(function() {
+    
+ let time = new Date()
+ 
+ let clock = document.getElementById('clock')
+ clock.innerHTML = time.toLocaleTimeString()   
+},1000)
+
+
+
+// data formattazione europea
+
+let dataEuropea = new Date (2022, 6 , 18 , 15, 56, 15, 1)
+console.log(dataEuropea)
+
+let dataFormattata = document.getElementById('data-europea')
+dataFormattata.innerHTML = dataEuropea
+
+
+// prove
 
 console.log (data.getDay())
 
-console.log (data.getMonth())
 
-console.log (data.getDate())
+if(data.getDay() == 0){
 
-console.log (data.toLocaleDateString())
+    console.log('Domenica')
+} else if ( data.getDay() == 1) {
+    console.log('Lunedì')
+} else if ( data.getDay() == 2) {
+    console.log('Martedì')
+} else if ( data.getDay() == 3) {
+    console.log('Mercoledì')
+} else if ( data.getDay() == 4) {
+    console.log('Giovedì')
+} else if ( data.getDay() == 5) {
+    console.log('Venerdì')
+} else if ( data.getDay() == 6) {
+    console.log('Sabato')
+} 
+
+// prove ora
 
 console.log (data.toLocaleTimeString())
-
-// formato europeo //
-
-
-let dataEuropea = new Date(2022, 6, 18, 15, 56, 15, 1)
-console.log (dataEuropea)
-
-let dataFormattata = document.getElementById("data-europea")
-dataFormattata.innerHTML= dataEuropea
-
-console.log (data.getDay())
-
-if(data.getDay==0) {
-
-    console.log ('Domenica')
-    
-
-}else if ( data.getDay == 1) {
-    console.log ('Lunedì')
-}
