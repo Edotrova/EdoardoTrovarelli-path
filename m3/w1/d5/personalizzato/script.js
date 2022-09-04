058fb57f434eb232145299cdd49977804d3c9f08
Edoardo Trovarelli <107913556+Edotrova@users.noreply.github.com>
2022-09-04 23:06:16 +0200

    personalizzato0409

---
+++ m3/w1/d5/personalizzato/script.js
@@ -0,0 +1,133 @@
+"use strict";
+// import {style} from 
+var _a, _b, _c, _d;
+class FirstUser {
+    //costruttore della classe
+    constructor(carica, numeroChiamate, costoChiamata, unaRicarica, minutiDurata) {
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoChiamata = costoChiamata;
+        this.unaRicarica = unaRicarica;
+        this.minutiDurata = minutiDurata;
+    }
+    ricarica(unaRicarica) {
+        this.carica += unaRicarica; //operatore di assegnamento +=
+    }
+    chiamata(minutiDurata) {
+        this.carica -= (this.costoChiamata * minutiDurata);
+        this.numeroChiamate++; //operatore di incremento per aumentare  
+        //il numero di chiamate
+    }
+    numero404() {
+        return this.carica;
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    }
+    azzeraChiamate() {
+        this.numeroChiamate -= this.numeroChiamate;
+    }
+}
+class SecondUser {
+    constructor(carica, numeroChiamate, costoChiamata, unaRicarica, minutiDurata) {
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoChiamata = costoChiamata;
+        this.unaRicarica = unaRicarica;
+        this.minutiDurata = minutiDurata;
+    }
+    ricarica(unaRicarica) {
+        this.carica += unaRicarica;
+    }
+    chiamata(minutiDurata) {
+        this.carica -= (this.costoChiamata * minutiDurata);
+        this.numeroChiamate++;
+    }
+    numero404() {
+        return this.carica;
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    }
+    azzeraChiamate() {
+        this.numeroChiamate -= this.numeroChiamate;
+    }
+}
+class ThirdUser {
+    constructor(carica, numeroChiamate, costoChiamata, unaRicarica, minutiDurata) {
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoChiamata = costoChiamata;
+        this.unaRicarica = unaRicarica;
+        this.minutiDurata = minutiDurata;
+    }
+    ricarica(unaRicarica) {
+        this.carica += unaRicarica;
+    }
+    chiamata(minutiDurata) {
+        this.carica -= (this.costoChiamata * minutiDurata);
+        this.numeroChiamate++;
+    }
+    numero404() {
+        return this.carica;
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    }
+    azzeraChiamate() {
+        this.numeroChiamate -= this.numeroChiamate;
+    }
+}
+//da qui in poi faccio l'istanza delle classi. 
+//Mario è l'istanza della classe FirstUser
+let Mario = new FirstUser(20, 5, 0.20, 30, 4);
+Mario.ricarica(30);
+(_a = document.getElementById('ricaricaEffettuata')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function mostraRicarica() {
+    alert("Hai effettuato una ricarica di " + Mario.unaRicarica + " euro ");
+});
+console.log(Mario.carica);
+(_b = document.getElementById('creditoResiduo')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', function mostraCredito() {
+    alert("Il tuo credito residuo è di " + Mario.carica + " euro ");
+});
+Mario.chiamata(2);
+console.log(Mario.carica);
+console.log(Mario.numeroChiamate);
+(_c = document.getElementById('chiamateEffettuate')) === null || _c === void 0 ? void 0 : _c.addEventListener('click', function mostraChiamate() {
+    alert("Da questa mattina hai effettuato " + Mario.numeroChiamate + " chiamate");
+});
+Mario.numero404();
+console.log(Mario.carica);
+Mario.getNumeroChiamate();
+console.log(Mario.numeroChiamate);
+// Mario.azzeraChiamate();
+// console.log(Mario.numeroChiamate)
+(_d = document.getElementById('ultimaChiamata')) === null || _d === void 0 ? void 0 : _d.addEventListener('click', function mostraUltimaChiamata() {
+    alert("La durata della tua ultima chiamata è stata di " + Mario.minutiDurata + " minuti");
+});
+//Laura è l'istanza della classe SecondUser
+let Laura = new FirstUser(10, 5, 0.20, 20, 4);
+Laura.ricarica(20);
+console.log(Laura.carica);
+Laura.chiamata(2);
+console.log(Laura.carica);
+console.log(Laura.numeroChiamate);
+Laura.numero404();
+console.log(Laura.carica);
+Laura.getNumeroChiamate();
+console.log(Laura.numeroChiamate);
+Laura.azzeraChiamate();
+console.log(Laura.numeroChiamate);
+//Francesco è l'istanza della classe ThirdUser.
+let Francesco = new FirstUser(5, 3, 0.20, 10, 3);
+Francesco.ricarica(20);
+console.log(Francesco.carica);
+Francesco.chiamata(40);
+console.log(Francesco.carica);
+console.log(Francesco.numeroChiamate);
+Francesco.numero404();
+console.log(Francesco.carica);
+Francesco.getNumeroChiamate();
+console.log(Francesco.numeroChiamate);
+Francesco.azzeraChiamate();
+console.log(Francesco.numeroChiamate);
+//# sourceMappingURL=script.js.map
---
+++ m3/w1/d5/personalizzato/script.ts
@@ -0,0 +1,265 @@
+
+// import {style} from 
+
+
+// document.querySelector(".button")!.addEventListener('click', function togliOpacita(){
+//     document.querySelector(".row")!.style.opacity = '5'
+// })
+
+
+interface ISmartphone{      //Qui ho costruito l'interfaccia 
+    carica:number;           //carica e numerochiamate sono le proprietà
+    numeroChiamate:number;
+     ricarica(unaRicarica:number):void;   //metodi
+     chiamata(minutiDurata:number):void;
+     numero404():number;
+     getNumeroChiamate(numeroChiamate:number):number;
+     azzeraChiamate(numeroChiamate:number):void;
+}
+
+
+class FirstUser implements ISmartphone {  //implementazione dell'interfaccia nella
+    carica:number;                        //classe
+    numeroChiamate:number;
+    costoChiamata:number;
+    unaRicarica:number;
+    minutiDurata:number;
+                                            //costruttore della classe
+    
+    constructor(carica:number, numeroChiamate:number, costoChiamata:number, unaRicarica:number, minutiDurata:number){
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;          
+        this.costoChiamata = costoChiamata;
+        this.unaRicarica = unaRicarica;
+        this.minutiDurata = minutiDurata;
+        
+    }
+
+ ricarica(unaRicarica:number):void {   // metodo ricarica()
+
+this.carica += unaRicarica  //operatore di assegnamento +=
+}
+
+chiamata(minutiDurata:number):void {  //metodo chiamata()
+
+    
+     this.carica -= (this.costoChiamata * minutiDurata) 
+
+     this.numeroChiamate++       //operatore di incremento per aumentare  
+                                 //il numero di chiamate
+}
+
+numero404():number {       
+
+return this.carica
+
+}
+
+getNumeroChiamate():number {
+    return this.numeroChiamate;
+}
+
+azzeraChiamate(): void {
+    this.numeroChiamate -= this.numeroChiamate; 
+}
+
+}
+
+
+class SecondUser implements ISmartphone {
+
+    carica:number;
+    numeroChiamate:number;
+    costoChiamata:number;
+    unaRicarica:number;
+    minutiDurata:number;
+
+    
+    constructor(carica:number, numeroChiamate:number, costoChiamata:number, unaRicarica:number, minutiDurata:number){
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoChiamata = costoChiamata;
+        this.unaRicarica = unaRicarica;
+        this.minutiDurata = minutiDurata;
+        
+    }
+
+ ricarica(unaRicarica:number):void {   //primo metodo
+
+this.carica += unaRicarica
+    
+}
+
+chiamata(minutiDurata:number):void {
+
+    
+     this.carica -= (this.costoChiamata * minutiDurata) 
+
+     this.numeroChiamate++
+
+}
+
+numero404():number {
+
+return this.carica
+
+}
+
+getNumeroChiamate():number {
+    return this.numeroChiamate;
+}
+
+azzeraChiamate(): void {
+    this.numeroChiamate -= this.numeroChiamate; 
+}
+
+}
+
+class ThirdUser implements ISmartphone {
+
+    carica:number;
+    numeroChiamate:number;
+    costoChiamata:number;
+    unaRicarica:number;
+    minutiDurata:number;
+
+    
+    constructor(carica:number, numeroChiamate:number, costoChiamata:number, unaRicarica:number, minutiDurata:number){
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoChiamata = costoChiamata;
+        this.unaRicarica = unaRicarica;
+        this.minutiDurata = minutiDurata;
+        
+    }
+
+ ricarica(unaRicarica:number):void {   //primo metodo
+
+this.carica += unaRicarica
+    
+}
+
+chiamata(minutiDurata:number):void {
+
+    
+this.carica -= (this.costoChiamata * minutiDurata) 
+
+this.numeroChiamate++
+
+}
+
+numero404():number {
+
+return this.carica
+
+}
+
+getNumeroChiamate():number {
+    return this.numeroChiamate;
+}
+
+azzeraChiamate(): void {
+    this.numeroChiamate -= this.numeroChiamate; 
+}
+
+}
+
+//da qui in poi faccio l'istanza delle classi. 
+//Mario è l'istanza della classe FirstUser
+
+
+let Mario = new FirstUser(20, 5, 0.20, 30, 4); 
+
+Mario.ricarica(30)
+
+document.getElementById('ricaricaEffettuata')?.addEventListener('click', function mostraRicarica(){
+    alert("Hai effettuato una ricarica di " + Mario.unaRicarica + " euro ")
+});
+
+console.log(Mario.carica)
+
+document.getElementById('creditoResiduo')?.addEventListener('click', function mostraCredito(){
+    alert("Il tuo credito residuo è di " + Mario.carica + " euro ")
+});
+
+
+Mario.chiamata(2)
+
+console.log(Mario.carica)
+console.log(Mario.numeroChiamate)
+document.getElementById('chiamateEffettuate')?.addEventListener('click', function mostraChiamate(){
+    alert("Da questa mattina hai effettuato " + Mario.numeroChiamate + " chiamate")
+});
+
+Mario.numero404()
+
+console.log(Mario.carica)
+
+Mario.getNumeroChiamate()
+
+console.log(Mario.numeroChiamate)
+
+
+// Mario.azzeraChiamate();
+
+// console.log(Mario.numeroChiamate)
+
+document.getElementById('ultimaChiamata')?.addEventListener('click', function mostraUltimaChiamata(){
+    alert("La durata della tua ultima chiamata è stata di " + Mario.minutiDurata + " minuti")
+});
+
+
+
+
+
+
+//Laura è l'istanza della classe SecondUser
+
+let Laura = new FirstUser(10, 5, 0.20, 20, 4);
+
+Laura.ricarica(20)
+
+console.log(Laura.carica)
+
+Laura.chiamata(2)
+
+console.log(Laura.carica)
+console.log(Laura.numeroChiamate)
+
+Laura.numero404()
+
+console.log(Laura.carica)
+
+Laura.getNumeroChiamate()
+
+console.log(Laura.numeroChiamate)
+
+Laura.azzeraChiamate();
+
+console.log(Laura.numeroChiamate)
+
+//Francesco è l'istanza della classe ThirdUser.
+
+let Francesco = new FirstUser(5, 3, 0.20, 10, 3);
+
+Francesco.ricarica(20)
+
+console.log(Francesco.carica)
+
+Francesco.chiamata(40)
+
+console.log(Francesco.carica)
+console.log(Francesco.numeroChiamate)
+
+Francesco.numero404()
+
+console.log(Francesco.carica)
+
+Francesco.getNumeroChiamate()
+
+console.log(Francesco.numeroChiamate)
+
+Francesco.azzeraChiamate();
+
+console.log(Francesco.numeroChiamate)
+
