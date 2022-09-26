017288f9fd2f6b28e570ff2fcabdb7d4c64ac78d
Edoardo Trovarelli <107913556+Edotrova@users.noreply.github.com>
2022-09-26 17:20:37 +0200

    consegna2609

---
+++ backend/EpicodeBackendCourse1/src/it/epicode/be/Esercizio1.java
@@ -0,0 +1,12 @@
+package it.epicode.be;
+
+public class Esercizio1 {
+public static void main(String[] args) {
+	int x = moltiplica(4,5);
+	System.out.println(x);
+}
+
+
+public static int moltiplica(int n1, int n2) {
+return n1 * n2;}
+}
